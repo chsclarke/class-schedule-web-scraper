a431386af0a3527ea9461303390f4460a402799a
Mio Diaz <themio@themioshow.com>
2022-09-09 23:05:39 -0400

    Google API + UNIX timestamp calc
    added google api and updated unix timestamp conversion function

--- get_schedule/get_schedule.py
+++ get_schedule/get_schedule.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
-import googleapiclient
-from httplib2 import Http
-from oauth2client import file, client, tools
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -9,9 +15,12 @@ from webdrivermanager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ExpectedConditions
 from selenium.webdriver.common.by import By
+
 import getpass
+
 import time
 import datetime as DateTime
+from datetime import date
 
 """
 Created on Tue Apr 25 18:45:37 2017
@@ -124,13 +133,15 @@ def get_pelo_data(credentials):
 
     WebDriverWait(driver, 7).until(ExpectedConditions.element_to_be_clickable((By.XPATH, "//div[@id='categories']/nav/div/div/a[2]"))).click()
 
-    # find all the dates and store that data for use later
-    date_time = DateTime.datetime.now()
-    # make time substring - remove 24hr portion of current time
-    unix_timestamp_substring = str(time.mktime(date_time.timetuple()))[:5]
-    # add in the 4 pm timestamp to the first 5 numbers in the timestamp 
-    # to create a timestamp with the current day but time of 4 pm
-    element_id_timestamp = unix_timestamp_substring + "09600"
+    # # find all the dates and store that data for use later
+    # date_time = DateTime.datetime.now()
+    # # make time substring - remove 24hr portion of current time
+    # unix_timestamp_substring = str(time.mktime(date_time.timetuple()))[:5]
+    # # add in the 4 pm timestamp to the first 5 numbers in the timestamp 
+    # # to create a timestamp with the current day but time of 4 pm
+    element_id_timestamp = str(get_unix_time())
+
+    print(element_id_timestamp)
 
     driver.find_element("name", "week-0")
     # loop through week-0 div and get dates
@@ -151,6 +162,28 @@ def get_pelo_data(credentials):
     driver.quit() #quitting the online session
     return return_list
 
+
+def get_unix_time():
+ # find all the dates and store that data for use later
+    date_time = date.today()
+
+    unix_time = DateTime.datetime(date_time.year, date_time.month, date_time.day, 00, 00)
+
+    # make time substring - remove 24hr portion of current time
+    element_id_timestamp = str(time.mktime(unix_time.timetuple()))[:10]
+    # add in the 4 pm timestamp to the first 5 numbers in the timestamp 
+    # to create a timestamp with the current day but time of 4 pm
+    # element_id_timestamp = unix_timestamp_substring + "09600"
+
+    # print(date_time)
+    # print(unix_time)
+    # print(element_id_timestamp)
+
+    return element_id_timestamp
+
+    # 1662696000
+    # 1662709600
+
     # Example output data
     # ['THURSDAY, SEPTEMBER 8\n7:00 PM\nLIVE\n20 min Tabata Ride\nKENDALL TOOLE · CYCLING', 'FRIDAY, SEPTEMBER 9\nNo Classes', 'SATURDAY, SEPTEMBER 10\nNo Classes', 'SUNDAY, SEPTEMBER 11\nNo Classes', 'MONDAY, SEPTEMBER 12\nNo Classes', 'TUESDAY, SEPTEMBER 13\nNo Classes', 'WEDNESDAY, SEPTEMBER 14\n12:35 PM\nLIVE\n20 min Low Impact Ride\nROBIN ARZÓN · CYCLING\nYOU’RE IN', 'THURSDAY, SEPTEMBER 15\n7:00 PM\nLIVE\n30 min 90s Pop Ride\nKENDALL TOOLE · CYCLING\nYOU’RE IN', 'FRIDAY, SEPTEMBER 16\n5:30 PM\nLIVE\n30 min Latin Ride\nROBIN ARZÓN · CYCLING\nYOU’RE IN', 'SATURDAY, SEPTEMBER 17\nNo Classes', 'SUNDAY, SEPTEMBER 18\nNo Classes', 'MONDAY, SEPTEMBER 19\nNo Classes', 'TUESDAY, SEPTEMBER 20\nNo Classes', 'WEDNESDAY, SEPTEMBER 21\nNo Classes']
 
@@ -166,37 +199,12 @@ def simplify(data): #simplifies the data that get_pelo_data returns
                 line = line+y
             if y == '\n': #replaces endl's with a space
                 line = line + " "
-        if(line[0:7] == "No Classes"):
-            return return_list
-        if(line != "" and line[0:7] != "No Classes"): #appends the edited data to return_list as long as its not empty or starting with 'Summer'
-            return_list.append(line)
+        # if(line[0:7] == "No Classes"):
+        #     return return_list
+        # if(line != "" and line[0:7] != "No Classes"): #appends the edited data to return_list as long as its not empty or starting with 'Summer'
+        #     return_list.append(line)
         line = ""
     return return_list
-
-def new_cal_event(summary, location, start_datetime, end_datetime): #adds an event to your calendar
-    print(summary,"||", location,"||", start_datetime,"||", end_datetime,"||", email)
-    EVENT = {
-        "summary": summary,
-        "location": location,
-        "start": {
-            "dateTime": start_datetime,
-            "timeZone": "America/New_York"
-        },
-        "end": {
-            "dateTime": end_datetime,
-            "timeZone": "America/New_York"
-        },
-        "attendees": [
-            {
-                "email": global_email,
-            },
-            # ...
-        ],
-    }
-    e = CAL.events().insert(calendarId='primary', sendNotifications=bool_send_email, body=EVENT).execute()
-    print('''*** %r event added:
-        Start: %s
-        End:   %s''' % (e['summary'].encode('utf-8'), e['start']['dateTime'], e['end']['dateTime']))
 
 # def start_to_militarty_time(i): 
 # #takes the start time that has been webscraped and turns it into military time
@@ -224,15 +232,16 @@ def new_cal_event(summary, location, start_datetime, end_datetime): #adds an eve
 
 #executing the functions
 credentials = login("Peloton") #getting username and password
-# send_email(credentials)#checking if user wants an email
+
 data = get_pelo_data(credentials) #scraping the website
 print(data)
 data = simplify(data) #simplifying data
-# data = parse(data) #making data usable
 print(data)
+calendar_api_call()
+
 
 #declaring the variables needed to call new_cal_event
-EVENT = {}
+event = {}
 
 start_time_of_day = ""
 end_time_of_day = ""
@@ -246,28 +255,94 @@ end_datetime = ""
 
 days_of_week = ""
 
-#start of google API code
-# try:
-#     import argparse
-#     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-# except ImportError:
-#     flags = None
-# SCOPES = 'https://www.googleapis.com/auth/calendar'
-# store = file.Storage('storage.json')
-# creds = store.get()
-# if not creds or creds.invalid:
-#     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-#     creds = tools.run_flow(flow, store, flags) \
-#             if flags else tools.run(flow, store)
-# CAL = build('calendar', 'v3', http=creds.authorize(Http()))
 
-#using the simplified and parsed data to add new calendar events
-# for i in data:
-#     length = len(i)
-#     summary = " ".join(i[5:-7])
-#     summary = summary + " " + str(i[-6]) + "-" + str(i[-7])
-#     start_time_of_day = start_to_militarty_time(i)
-#     end_time_of_day = end_to_military_time(i)
-#     days_of_week = i[0]
+### TODO
+## Research google api
+## determine how build event for calendar event
+## create function to pull duration of class and time of class to create end_time var
+## determine what part will go into the event name ++ summary 
+###
+
+## https://developers.google.com/calendar/api/v3/reference
+def new_cal_event(): 
+    # Build event response body for calendar event for each class in schedule
+    event = {
+      'summary': 'Google I/O 2015',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2015-05-28T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+        'dateTime': '2015-05-28T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+      ],
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+
+#start of google API code
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+def calendar_api_call():
+    creds = None
+
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+
+        # events_result = service.events().list(calendarId='primary', timeMin=now,
+        #                                       maxResults=10, singleEvents=True,
+        #                                       orderBy='startTime').execute()
+        # events = events_result.get('items', [])
+
+        # if not events:
+        #     print('No upcoming events found.')
+        #     return
+
+        # # Prints the start and name of the next 10 events
+        # for event in events:
+        #     start = event['start'].get('dateTime', event['start'].get('date'))
+        #     print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
 
 print("\nYour schedule has been updated. Check your google calendar.\n")
